b65f301c67e4c9bb393034fef91f1fb6ee939462
redteam-SP <anushadevelopment@gmail.com>
2021-08-19 18:37:52 +0800

    dfsdf

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ from jinja2 import exceptions
 import json
 from model.User import User
 from validation.Validataor import *
+from flask_cors import CORS
 
 app=Flask(__name__) 
+CORS(app)
 
 @app.route('/')
 def showDemopage():
