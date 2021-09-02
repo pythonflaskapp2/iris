da3f91d5ab20837b5ae6a6ffb43679caf292f07c
redteam-SP <anushadevelopment@gmail.com>
2021-09-02 14:40:45 +0800

    databse change

--- config/Settings.py
+++ config/Settings.py
@@ -3,7 +3,7 @@ import os
 class Settings:
     secretKey = "321897kdbmgj321djnbug#@4rhxcjk6(!d)"
     
-   # host = os.environ['HOST']
-    #database = os.environ['DATABASE']
-    #user = os.environ['USERNAME']
-    #password = os.environ['PASSWORD']
+    host = os.environ['HOST']
+    database = os.environ['DATABASE']
+    user = os.environ['USERNAME']
+    password = os.environ['PASSWORD']
